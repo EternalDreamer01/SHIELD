9b4ed835b96fe3ef6a45b9d661c4275b6b76f576
Dimitri Simon <dimitri.simon@telecom-sudparis.eu>
2026-07-02 11:26:06 +0200

    docs: describe steps

--- src/main.py
+++ src/main.py
@@ -7,19 +7,23 @@ from srcEx.sub import Sub
 import threading
 import time
 from analysis_experiments import runGraphs
+from rich.progress import track
 
-if __name__ == "__main__":   
-    numberExp=2  # number of experiments run 
+
+if __name__ == "__main__":
+    numberExp=2 # number of experiments
     td=1 # experiment duration
 
     pubs=runPublish()
 
     print('STARTING THE EXPERIMENT BEFORE THE SECURITY MODULE')
 
+	##################################################
+	# 1. Publish messages
+	##################################################
     for _ in range(numberExp):
         threads=[]
         for pub,client in pubs:
-            
             threads.append(threading.Thread(target=pub.publish, args=(td,client,)))
         
         for thread in threads:
@@ -27,14 +31,16 @@ if __name__ == "__main__":
 
         time.sleep(td+2)
         
-        #pub=Pub("end", topic='endtopic', broker='172.18.0.2', port=1883)
+        #pub=Pub("end", topic='endtopic', broker='127.0.0.1', port=1883)
         #pub.publishEnd()
     
+	##################################################
+	# 2. Retrieve network and generate attack graph
+	##################################################
     file_network="data/v2x_network.json"
 
-    if not os.path.exists("experiments"): os.mkdir("experiments")
-    if not os.path.exists("experiments/data"): os.mkdir("experiments/data")
-    if not os.path.exists("experiments/plot"): os.mkdir("experiments/plot")
+    os.makedirs("experiments/data", exist_ok=True)
+    os.makedirs("experiments/plot", exist_ok=True)
 
     with open(file_network) as nf:
         devices = json.load(nf)["devices"]
@@ -43,10 +49,13 @@ if __name__ == "__main__":
 
     AG = generate_ag_model(file_network)
 
+	######################################################################
+	# 3. Generate paths, analyze them, calculate trust and mitigations,
+	#    and save the attack surface and unsubscriptions for each device
+	######################################################################
     tot_surface=[]
     tot_unsubscribe=[]
-    for id in devs:
-
+    for id in track(devs):
         src_dev=[id]
         paths = generate_paths(file_network, AG, src_dev)
         risk_vals = analyze_paths(paths,file_network)
@@ -59,9 +68,11 @@ if __name__ == "__main__":
         tot_unsubscribe.append(unsubscriptions)
 
     data_unsub=[]
-   
-    for elem in tot_unsubscribe:
-        
+
+	######################################################################
+	# 4. Save the unsubscriptions
+	######################################################################
+    for elem in track(tot_unsubscribe):
         for keypath in elem.keys():
             src=keypath.split("#")[0]
             dst=keypath.split("#")[1]
@@ -69,10 +80,8 @@ if __name__ == "__main__":
             data_unsub.append({
                 "src":src,
                 "dst":dst,
-                "unsubscription":val    
+                "unsubscription":val
             })
-	
-
 
     keys = data_unsub[0].keys()
     with open('experiments/data/unsubscriptions.csv', 'w', newline='') as output_file:
@@ -81,20 +90,22 @@ if __name__ == "__main__":
         dict_writer.writerows(data_unsub)
     
     time.sleep(td+2)
-    pubendsec=Pub("endsec", topic='endtopic', broker='172.18.0.2', port=1883)
+    pubendsec=Pub("endsec", topic='endtopic', broker='127.0.0.1', port=1883)
     pubendsec.publishEnd()
 
     print('STARTING THE EXPERIMENT')
 
-    for _ in range(numberExp):
+	######################################################################
+	# 5. Run the experiment by pusbling the messages again
+	######################################################################
+    for _ in track(range(numberExp)):
         threads=[]
         for pub,client in pubs:
-            
             threads.append(threading.Thread(target=pub.publishSecExp, args=(td,client,)))
         
         for thread in threads:
             thread.start()
-        for thread in threads:
+        for thread in track(threads):
             thread.join()
         time.sleep(td+2)  # flush the messages queue
                
